c19c9395c0c1fd6387f93e9d1505194b3c8c6833
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-07-31 22:15:12 +0300

    ADDED: try catch in render functions

--- src/js/helpers/renderStatistic.js
+++ src/js/helpers/renderStatistic.js
@@ -6,30 +6,34 @@ import { chooseIcon, calculateStatistic } from '../helpers'
 const statisticList = document.querySelector('.render-statistic')
 
 export function renderStatistic() {
-	calculateStatistic()
+	try {
+		calculateStatistic()
 
-	const markup = statisticData.reduce((acc, stat) => {
-		const icon = chooseIcon(stat.category);
-		if (stat.active === 0 && stat.archived === 0) {
-			return acc + '';
-		}
-		return acc + `
-			<li class="table__item">
-				<ul class="table-row">
-					<li class="table-row__item">
-						<div class="icon-wrapper">
-							<svg class="table-icon" width="24" height="24">
-								<use href="${icon}"></use>
-							</svg>
-						</div>
-						<span>${stat.category}</span>
-					</li>
-					<li class="table-row__item">${stat.active}</li>
-					<li class="table-row__item">${stat.archived}</li>
-				</ul>
-			</li>
-		`
-	}, '')
+		const markup = statisticData.reduce((acc, stat) => {
+			const icon = chooseIcon(stat.category);
+			if (stat.active === 0 && stat.archived === 0) {
+				return acc + '';
+			}
+			return acc + `
+				<li class="table__item">
+					<ul class="table-row">
+						<li class="table-row__item">
+							<div class="icon-wrapper">
+								<svg class="table-icon" width="24" height="24">
+									<use href="${icon}"></use>
+								</svg>
+							</div>
+							<span>${stat.category}</span>
+						</li>
+						<li class="table-row__item">${stat.active}</li>
+						<li class="table-row__item">${stat.archived}</li>
+					</ul>
+				</li>
+			`
+		}, '')
 
-	statisticList.innerHTML = markup;
+		statisticList.innerHTML = markup;
+	} catch (error) {
+		console.log(error)
+	}
 }
